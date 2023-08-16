6e778570069e797256fa3c56f7228c013331f20b
João Vitor <joaovitorcfdev@gmail.com>
2023-08-16 16:34:14 -0300

    implementing data layer

---
+++ lib/layers/data/dto/carro_dto.dart
@@ -0,0 +1,33 @@
+import 'package:arq/layers/domain/entities/carro_entity.dart';
+
+class CarroDto extends CarroEntity {
+  final String placa;
+  final int quantidadeDePortas;
+  final double valorFinal;
+
+  CarroDto({
+    required this.placa,
+    required this.quantidadeDePortas,
+    required this.valorFinal,
+  }) : super(
+          placa: placa,
+          qtdPortas: quantidadeDePortas,
+          valor: valorFinal,
+        );
+
+  Map toMap() {
+    return {
+      'placa': placa,
+      'quantidade_de_portas': qtdPortas,
+      'valor_final': valorFinal,
+    };
+  }
+
+  CarroDto fromMap(Map map) {
+    return CarroDto(
+      placa: map['placa'],
+      quantidadeDePortas: map['quantidade_de_portas'],
+      valorFinal: map['valor_final'],
+    );
+  }
+}
